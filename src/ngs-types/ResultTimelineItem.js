f3490b2d66bc1779f6bbb043795ff28907e27f8f
Ilya Sher <34807039+ilyash-b@users.noreply.github.com>
2025-12-28 06:36:02 +0400

    Fixed font for scalar (including text) output

--- src/ngs-types/ResultTimelineItem.js
+++ src/ngs-types/ResultTimelineItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Scalar } from "./Scalar";
 
 export class ResultTimelineItem {
     constructor({children}) {
@@ -6,6 +7,17 @@ export class ResultTimelineItem {
     }
 
     toWidget() {
+
+        if (this.children.length === 1 && this.children[0] instanceof Scalar) {
+            return <div className="ResultTimelineItem">
+                <pre>
+                    {
+                        this.children.map(c => c.toWidget())
+                    }
+                </pre>
+            </div>
+        }
+
         return <div className="ResultTimelineItem">
             {
                 this.children.map(c => c.toWidget())
